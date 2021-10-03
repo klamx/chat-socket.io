87f86fae01ea9331de073b5717b8daa390e1a82c
Alejandro Largo <largoalejo@gmail.com>
2021-10-03 12:03:58 -0500

    chat con express y socket.io

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// settings
+app.set('port', process.env.PORT || 3000);
+
+// static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// start the server
+const server = app.listen(app.get('port'), () => {
+  console.log('server on port', app.get('port'));
+});
+
+// websockets
+const SocketIO = require('socket.io');
+const io = SocketIO(server);
+io.on('connection', (socket) => {
+  // console.log('new conection', socket.id);
+  socket.on('chat:message', (data) => {
+    io.sockets.emit('chat:message', data);
+  });
+
+  socket.on('chat:typing', (data) => {
+    socket.broadcast.emit('chat:typing', data);
+  });
+});
